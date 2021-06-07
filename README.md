651554e1c9f07bdc2b06897dc18a64760f6dd25c
M-Jay <1376031164@qq.com>
2021-06-07 23:08:02 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # micropython-urouter
- A lightweight HTTP request routing processing support library based on micropython.  The previous name was micro-route
+ A lightweight HTTP request routing processing support library based on micropython.  
+ The previous name was [micro-route](https://github.com/Li-Lian1069/micro_route)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='micropython-urouter',
+    version='0.1',
+    packages=['urouter'],
+    #package_dir = {'djangoforandroid': 'djangoforandroid'},
+
+    author='M-Jay',
+    author_email='m-jay-1376@qq.com',
+    maintainer='M-Jay',
+    maintainer_email='m-jay-1376@qq.com',
+
+    #url = 'http://www.pinguino.cc/',
+    url='http://urouter.m-jay.cn/',
+    download_url='https://github.com/Li-Lian1069/micropython-urouter/releases',
+
+    install_requires=[],
+
+    license='GNU LGPL v3',
+    description="A lightweight web server",
+    #    long_description = README,
+    keywords="micropython",
+
+    classifiers=[
+        'Environment :: Web Environment',
+        # 'Framework :: Django',
+        'Development Status :: 3 - Alpha'
+    ],
+
+)
