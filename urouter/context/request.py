c4a003db9340015d9732e07f8b7656786a07542b
2814350255 <40719635+2814350255@users.noreply.github.com>
2023-01-16 18:48:36 +0800

    Update request.py

--- urouter/context/request.py
+++ urouter/context/request.py
@@ -69,7 +69,9 @@ class Request():
         # comp http first line.
         self.host, self.port = addr
 
-        self._form = None
+        self._form = {}
+        
+        self.buffsize = None
 
         self.method, self.uri, self.http_version = head
 
@@ -176,7 +178,7 @@ class Request():
         # content = data.decode(CONFIG.charset)
         # items = content.split("&")
         # try to recv all data.
-        items: list = self._client.recv()\
+        items: list = self._client.recv(self.buffsize)\
             .decode(CONFIG.charset)\
             .split("&")
         collect()
